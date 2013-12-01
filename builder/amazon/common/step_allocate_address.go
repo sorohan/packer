172627f41a3f4a5e9a87642c8d2df0658a41d430
Ben Sorohan <ben.sorohan@bluehype.com.au>
2013-12-01 21:06:02 +1000

    Cleanup after elastic IP allocation.

--- builder/amazon/common/step_allocate_address.go
+++ builder/amazon/common/step_allocate_address.go
@@ -13,9 +13,11 @@ import (
 // Produces:
 //   addressAllocationId: string - The ID of the address allocation.
 //   addressAssociationId: string - The ID of the address association.
+//   publicIp: string - The Public IP of the instance.
 type StepAllocateAddress struct {
-	addressAllocationId  string
+	addressAllocationId string
 	addressAssociationId string
+    publicIp string
 }
 
 func (s *StepAllocateAddress) Run(state multistep.StateBag) multistep.StepAction {
@@ -42,18 +44,20 @@ func (s *StepAllocateAddress) Run(state multistep.StateBag) multistep.StepAction
 		return multistep.ActionHalt
 	}
 
-	// Set the allocation ID so we remember to deallocate it later
+	// Set the allocation ID so we remember to deallocate it later.
 	s.addressAllocationId = allocateAddressResp.AllocationId
 	log.Printf("Address Allocation ID: %s", s.addressAllocationId)
 
-	ui.Say("Associating new EIP...")
+    // Set the public IP so we can connect to it later.
+    s.publicIp = allocateAddressResp.PublicIp
+
+	ui.Say(fmt.Sprintf("Associating new EIP: %s...", allocateAddressResp.PublicIp))
 	// Associate the EIP with the VPC instance.
 	associateAddress := &ec2.AssociateAddress{
 		InstanceId:         instance.InstanceId,
 		AllocationId:       allocateAddressResp.AllocationId,
 		AllowReassociation: false,
 	}
-    ui.Say(fmt.Sprintf("Instance Id: %s, Allocation Id: %s", associateAddress.InstanceId, associateAddress.AllocationId));
 	associateAddressResp, err := ec2conn.AssociateAddress(associateAddress)
 
 	if err != nil {
@@ -69,23 +73,30 @@ func (s *StepAllocateAddress) Run(state multistep.StateBag) multistep.StepAction
 	log.Printf("Address Association ID: %s", s.addressAssociationId)
 
 	state.Put("address_allocation_id", s.addressAllocationId)
+    state.Put("public_ip", s.publicIp)
 	state.Put("address_associate_id", s.addressAssociationId)
 	return multistep.ActionContinue
 }
 
 func (s *StepAllocateAddress) Cleanup(state multistep.StateBag) {
-	// TODO: Cleanup address.
-	if s.addressAllocationId == "" {
-		return
+	ec2conn := state.Get("ec2").(*ec2.EC2)
+	ui := state.Get("ui").(packer.Ui)
+
+    if s.addressAssociationId != "" {
+        ui.Say("Disassociating the EIP...")
+		_, err := ec2conn.DisassociateAddress(s.addressAssociationId)
+		if err != nil {
+			ui.Error(fmt.Sprintf("Error disassociating EIP: %s", err))
+		}
+    }
+
+	if s.addressAllocationId != "" {
+        ui.Say("Releasing the EIP...")
+		_, err := ec2conn.ReleaseAddress(s.addressAllocationId)
+		if err != nil {
+			ui.Error(fmt.Sprintf("Error releasing EIP: %s", err))
+		}
 	}
+
 	return
-
-	//	ec2conn := state.Get("ec2").(*ec2.EC2)
-	//	ui := state.Get("ui").(packer.Ui)
-
-	//	ui.Say("Deleting the created EBS volume...")
-	//	_, err := ec2conn.DeleteVolume(s.addressAllocationId)
-	//	if err != nil {
-	//		ui.Error(fmt.Sprintf("Error deleting EBS volume: %s", err))
-	//	}
 }
